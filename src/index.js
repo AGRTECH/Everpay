4050f9a7f32f168d99a705a7c2942147e413be54
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2022-12-03 16:26:14 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -13,14 +13,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-App.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    App.settings.env
-  );
-});
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
