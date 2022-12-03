a4564528423277fdb24b8a2be547cb8468462e63
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2022-12-03 16:09:11 -0500

    heroku

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ App.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
-    app.settings.env
+    App.settings.env
   );
 });
 
