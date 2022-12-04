826d5f3d804c0edae7fb20ab714c36a476420bcf
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2022-12-04 16:52:26 -0500

    create stream not showing in ui

--- src/components/CreateStream.js
+++ src/components/CreateStream.js
@@ -64,130 +64,130 @@ const CreateStream = (props) => {
 
   return (
     <>
-      {everpayLoaded && tetherLoaded ? (
-        <div className="container-1 shadow">
-          <p>Tap the button below to create a stream</p>
-          <div className="bar"></div>
-          {/* <button
+      {/* {everpayLoaded && tetherLoaded ? ( */}
+      <div className="container-1 shadow">
+        <p>Tap the button below to create a stream</p>
+        <div className="bar"></div>
+        {/* <button
             onClick={() => {
               showBalances(dispatch, account, tether, everpay);
             }}
           >
             Show Balances
           </button> */}
-          <Button className="stream-button" onClick={handleShow}>
-            Stream!
-          </Button>
-          <Modal
-            show={show}
-            onHide={handleClose}
-            backdrop="static"
-            keyboard={false}
-          >
-            <Modal.Header className="modal-title" closeButton>
-              <Modal.Title className="modal-title">Create Stream</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <form
-                className=""
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  approveFunds(everpay, tether, deposit, account, dispatch);
+        <Button className="stream-button" onClick={handleShow}>
+          Stream!
+        </Button>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header className="modal-title" closeButton>
+            <Modal.Title className="modal-title">Create Stream</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form
+              className=""
+              onSubmit={(e) => {
+                e.preventDefault();
+                approveFunds(everpay, tether, deposit, account, dispatch);
+              }}
+            >
+              <p>What is the total amount you will stream?</p>
+              <input
+                type="text"
+                placeholder="1000 Tether"
+                onChange={(e) => {
+                  dispatch(streamAmountChanged(parseInt(e.target.value)));
                 }}
-              >
-                <p>What is the total amount you will stream?</p>
-                <input
-                  type="text"
-                  placeholder="1000 Tether"
-                  onChange={(e) => {
-                    dispatch(streamAmountChanged(parseInt(e.target.value)));
-                  }}
-                  className="bg-dark text-white form-group stream-amount mr-2"
-                  required
-                />
-                <p>What address will be receiving this stream?</p>
-                <input
-                  type="text"
-                  placeholder="0x0000..."
-                  onChange={(e) => {
-                    if (
-                      e.target.value.length < 42 ||
-                      e.target.value[0] !== "0" ||
-                      e.target.value[1] !== "x"
-                    ) {
-                      return;
-                    } else {
-                      dispatch(recipientAddressChanged(e.target.value));
-                    }
-                  }}
-                  className="bg-dark text-white form-group stream-amount"
-                  required
-                />
-                <Dropdown className="form-group">
-                  <p>What token will be streamed?</p>
-                  <Dropdown.Toggle
-                    variant="success"
-                    className="dropdown-buttons time-amount"
-                    id="dropdown-basic"
-                  >
-                    Token
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item
-                      href="#/action-1"
-                      onClick={(e) => {
-                        dispatch(tokenChanged(tether._address));
-                      }}
-                    >
-                      Tether
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                <p>For how long should the tokens be streamed?</p>
-                <Dropdowns />
-                <p className="shadow pl-2">{`Final Time Interval: ${
-                  endTime
-                    ? `D: ${textDay} H: ${textHour} M: ${textMinute} S: ${textSecond}`
-                    : "D: 0  H: 0  M: 0 S: 0"
-                }`}</p>
-                <button
-                  className="btn btn-primary stream-button form-group"
-                  type="submit"
+                className="bg-dark text-white form-group stream-amount mr-2"
+                required
+              />
+              <p>What address will be receiving this stream?</p>
+              <input
+                type="text"
+                placeholder="0x0000..."
+                onChange={(e) => {
+                  if (
+                    e.target.value.length < 42 ||
+                    e.target.value[0] !== "0" ||
+                    e.target.value[1] !== "x"
+                  ) {
+                    return;
+                  } else {
+                    dispatch(recipientAddressChanged(e.target.value));
+                  }
+                }}
+                className="bg-dark text-white form-group stream-amount"
+                required
+              />
+              <Dropdown className="form-group">
+                <p>What token will be streamed?</p>
+                <Dropdown.Toggle
+                  variant="success"
+                  className="dropdown-buttons time-amount"
+                  id="dropdown-basic"
                 >
-                  Approve Funds
-                </button>
-              </form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button
-                className="stream-button"
-                variant="primary"
-                onClick={() => {
-                  createStreamFunc(
-                    dispatch,
-                    everpay,
-                    account,
-                    receiver,
-                    deposit,
-                    streamToken,
-                    endTime,
-                    approved
-                  );
-                  handleClose();
-                }}
+                  Token
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    href="#/action-1"
+                    onClick={(e) => {
+                      dispatch(tokenChanged(tether._address));
+                    }}
+                  >
+                    Tether
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <p>For how long should the tokens be streamed?</p>
+              <Dropdowns />
+              <p className="shadow pl-2">{`Final Time Interval: ${
+                endTime
+                  ? `D: ${textDay} H: ${textHour} M: ${textMinute} S: ${textSecond}`
+                  : "D: 0  H: 0  M: 0 S: 0"
+              }`}</p>
+              <button
+                className="btn btn-primary stream-button form-group"
+                type="submit"
               >
-                Finalize
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      ) : (
+                Approve Funds
+              </button>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button
+              className="stream-button"
+              variant="primary"
+              onClick={() => {
+                createStreamFunc(
+                  dispatch,
+                  everpay,
+                  account,
+                  receiver,
+                  deposit,
+                  streamToken,
+                  endTime,
+                  approved
+                );
+                handleClose();
+              }}
+            >
+              Finalize
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      {/* ) : (
         <div>Contracts Not loaded</div>
-      )}
+      )} */}
     </>
   );
 };
